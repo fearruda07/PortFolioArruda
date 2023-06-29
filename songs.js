c3fd96383e1bfe883ebfc5192d9a1081fb6f61f8
Arruda <Felipe.Arruda@externals.adidas.com>
2023-06-29 02:38:02 -0300

    primeiro commit

---
+++ songs.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    src: "./src/james Da Salada De Fruta.mp3",
+    name: "James Da uma salada de frutas",
+  },
+  {
+    src: "./src/Zé da Manga - Sound Effects BR.mp3",
+    name: "Zé da manga",
+  },
+  {
+    src: "./src/Girl Before A Mirror - Verified Picasso.mp3",
+    name: "Girl Before A Mirror - Verified Picasso",
+  },
+  {
+    src: "./src/Like That - Anno Domini Beats.mp3",
+    name: "Like That - Anno Domini Beats",
+  },
+  {
+    src: "./src/Shake - Anno Domini Beats.mp3",
+    name: "Shake - Anno Domini Beats",
+  },
+  {
+    src: "./src/Turn Up Burn Up - Diamond Ortiz.mp3",
+    name: "Turn Up Burn Up - Diamond Ortiz",
+  },
+  {
+    src: "./src/Yo Picasso - Verified Picasso.mp3",
+    name: "Yo Picasso - Verified Picasso",
+  },
+];
